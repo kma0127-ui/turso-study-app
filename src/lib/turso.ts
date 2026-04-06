e8be5614e0cb8b7e30038f12b71ffa9d045c2539
EST <EST@ma1234.local>
2026-04-06 16:48:30 +0900

    feat: turso list & insert setup

---
+++ src/lib/turso.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@libsql/client";
+
+const url = process.env.TURSO_DATABASE_URL;
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+export const turso = createClient({
+  url: url || "file:./local.db",
+  authToken: authToken,
+});
